70b732610d70e26760e36574f252d912a89220ef
Oren Samuel <orensam@cs.huji.ac.il>
2014-03-03 17:33:05 +0200

    Adding initial files - mid work on project 4

---
+++ projects/04/fill/Fill.asm
@@ -0,0 +1,84 @@
+// This file is part of www.nand2tetris.org
+
+// and the book "The Elements of Computing Systems"
+
+// by Nisan and Schocken, MIT Press.
+
+// File name: projects/04/Fill.asm
+
+
+
+// Runs an infinite loop that listens to the keyboard input. 
+
+// When a key is pressed (any key), the program blackens the screen,
+
+// i.e. writes "black" in every pixel. When no key is pressed, the
+
+// program clears the screen, i.e. writes "white" in every pixel.
+
+
+
+(INFINITE_LOOP)
+    @SCREEN
+    D = A
+    @i
+    M = D
+
+    @KBD
+    D = M
+
+    @BLACK_LOOP
+    D; JGT
+
+    @WHITE_LOOP
+    D; JEQ
+
+(BLACK_LOOP)
+
+    D = -1
+    @i
+    A = M
+    M = D
+
+    @i
+    M = M + 1
+
+    @KBD
+    D = A
+
+    @i
+    D = D - M
+
+    @BLACK_LOOP
+    D; JGT
+
+    @INIFINITE_LOOP
+    0; JMP
+
+(WHITE_LOOP)
+
+    D = 0
+
+    @i
+    A = M
+    M = D
+
+    @i
+    M = M + 1
+
+    @KBD
+    D = A
+    @i
+    D = D - M
+
+    @WHITE_LOOP
+    D; JGT
+
+    @INIFINITE_LOOP
+    0; JMP
+
+
+
+
+
+
