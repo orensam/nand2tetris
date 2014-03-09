4f366fae7b28a5c66b9dc477627c829e3e155f9f
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-03-09 16:45:40 +0200

    finished memory.hdl

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,84 +1,80 @@
 // This file is part of www.nand2tetris.org
-
 // and the book "The Elements of Computing Systems"
-
 // by Nisan and Schocken, MIT Press.
-
 // File name: projects/04/Fill.asm
 
-
-
 // Runs an infinite loop that listens to the keyboard input. 
-
 // When a key is pressed (any key), the program blackens the screen,
-
 // i.e. writes "black" in every pixel. When no key is pressed, the
-
 // program clears the screen, i.e. writes "white" in every pixel.
 
+(LOOP)
 
-
-(INFINITE_LOOP)
+    // Save address of SCREEN in i
     @SCREEN
     D = A
     @i
     M = D
 
+    // Check keybaord input and turn screen
+    // white / black accordingly
     @KBD
     D = M
-
     @BLACK_LOOP
     D; JGT
-
     @WHITE_LOOP
     D; JEQ
 
 (BLACK_LOOP)
 
+    // Put black pixel
     D = -1
     @i
     A = M
     M = D
 
+    // Go to next pixel
     @i
     M = M + 1
 
+    // Check if at end of pixell array
+    // (KBD adresss is the end of the screen buffer
     @KBD
     D = A
-
     @i
     D = D - M
 
+    // If more pixels need to turn black, continue loop
     @BLACK_LOOP
     D; JGT
 
-    @INIFINITE_LOOP
+    // Finished coloring, back to main loop
+    @LOOP
     0; JMP
 
 (WHITE_LOOP)
 
+    // Put white pixel
     D = 0
-
     @i
     A = M
     M = D
 
+    // Go to next pixel
     @i
     M = M + 1
 
+    // Check if at end of pixell array
+    // (KBD adresss is the end of the screen buffer
     @KBD
     D = A
     @i
     D = D - M
 
+    // If more pixels need to turn white, continue loop
     @WHITE_LOOP
     D; JGT
 
-    @INIFINITE_LOOP
+    // Finished coloring, back to main loop
+    @LOOP
     0; JMP
-
-
-
-
-
-
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,14 +6,17 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-
+// Loop counter
 @i
 M=0
 
+// initialize result to 0
 @R2
 M=0
 
 (LOOP)
+    // Addition loop
+    // increase counter
     @i
     M=M+1
     D=M
@@ -21,13 +24,15 @@ M=0
     D=D-M
 
     @END
-    D; JGT // If d==0 goto END
+    D; JGT // If d==0 (i.e finished R0 loops) goto END
 
+    // Add R1 to result
     @R1
     D=M
     @R2
     M=M+D
 
+    // Repeat
     @LOOP
     0;JMP
 
